3c26134661bd9e81aa68616740b89616738139ea
Aakanksha <akanshi2007@gmail.com>
2026-09-12 16:40:19 +0530

    .

---
+++ class_problems/Class65.java
@@ -0,0 +1,34 @@
+package feature_session_6.class_problems;
+
+class Student {
+
+    String name;
+    double attendance;
+
+    static String collegeName =
+        "SRM Institute of Science and Technology";
+
+    static int studentCount = 0;
+
+    Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students: " + studentCount);
+    }
+}
+
+public class Class65 {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Ravi", 90);
+        Student s2 = new Student("Anitha", 95);
+
+        Student.printCollegeInfo();
+    }
+}
